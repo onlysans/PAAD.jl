57bf8dc7b20811e269371a980af61878e63ede71
Sayantan Das <36279638+ucalyptus@users.noreply.github.com>
2020-09-05 18:48:32 +0530

    Update tr_mul.jl

--- benchmark/others/tr_mul.jl
+++ benchmark/others/tr_mul.jl
@@ -2,12 +2,12 @@ using BenchmarkTools, LinearAlgebra
 import Zygote
 import Zygote: @grad
 using Test
-import AutoGrad, YAAD
+import AutoGrad, PAAD
 
 Zygote.@grad LinearAlgebra.tr(x) = LinearAlgebra.tr(x), Δ-> (Δ * Matrix(I, size(x)), )
 Zygote.@grad Base.:(*)(lhs::Matrix, rhs::Matrix) = gemm(lhs, rhs), grad -> (grad * transpose(rhs), transpose(lhs) * grad)
 
-function bench_tr_mul_yaad(x1, x2)
+function bench_tr_mul_paad(x1, x2)
     z = tr(x1 * x2)
     YAAD.backward(z)
     x1.grad, x2.grad
@@ -24,20 +24,20 @@ end
 
 xv, yv = rand(30, 30), rand(30, 30)
 
-yaad_x = YAAD.Variable(xv)
-yaad_y = YAAD.Variable(yv)
+paad_x = PAAD.Variable(xv)
+paad_y = PAAD.Variable(yv)
 
 autograd_x = AutoGrad.Param(xv)
 autograd_y = AutoGrad.Param(yv)
 
 println("Check gradient:")
 
-@test bench_tr_mul_yaad(yaad_x, yaad_y) == bench_tr_mul_autograd(autograd_x, autograd_y)
+@test bench_tr_mul_paad(paad_x, paad_y) == bench_tr_mul_autograd(autograd_x, autograd_y)
 
 #  == bench_tr_mul_zygote(xv, yv)
 println("----------------------------------------------------------------------")
-println("YAAD:")
-display(@benchmark bench_tr_mul_yaad(yaad_x, yaad_y))
+println("PAAD:")
+display(@benchmark bench_tr_mul_paad(paad_x, paad_y))
 
 println()
 println("----------------------------------------------------------------------")
