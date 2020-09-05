92f9cad7fec32f89147fae414e0bc8c99c5a9cf7
Sayantan Das <36279638+ucalyptus@users.noreply.github.com>
2020-09-05 18:10:05 +0530

    Update tr_mul.jl

--- benchmark/tr_mul.jl
+++ benchmark/tr_mul.jl
@@ -1,5 +1,5 @@
 using Test, BenchmarkTools, LinearAlgebra
-import YAAD
+import PAAD
 
 function bench_tr_mul_base(x1, x2)
     z1 = x1 * x2
@@ -13,45 +13,45 @@ end
 #     z1 = x1 * x2
 #     z2 = tr(z1)
 #
-#     grads = YAAD.gradient(tr, one(eltype(x1)), z2, z1)
-#     YAAD.gradient(*, first(grads), z1, x1, x2)
+#     grads = PAAD.gradient(tr, one(eltype(x1)), z2, z1)
+#     PAAD.gradient(*, first(grads), z1, x1, x2)
 # end
 
 function bench_tr_mul_yaad(x1, x2)
     z = tr(x1 * x2)
-    YAAD.backward(z)
+    PAAD.backward(z)
     x1.grad, x2.grad
 end
 
 function bench_tr_mul_yaad_static(z, x1, x2)
-    YAAD.forward(z)
-    YAAD.backward(z)
+    PAAD.forward(z)
+    PAAD.backward(z)
     x1.grad, x2.grad
 end
 
 xv, yv = rand(30, 30), rand(30, 30)
 
-yaad_x = YAAD.Variable(xv)
-yaad_y = YAAD.Variable(yv)
+yaad_x = PAAD.Variable(xv)
+yaad_y = PAAD.Variable(yv)
 
 @benchmark bench_tr_mul_yaad(yaad_x, yaad_y)
 
 z = tr(yaad_x * yaad_y)
-YAAD.forward(z)
+PAAD.forward(z)
 
 @benchmark bench_tr_mul_yaad_static(z, yaad_x, yaad_y)
 
 @benchmark bench_tr_mul_base(xv, yv)
 
 println("----------------------------------------------------------------------")
-println("YAAD:")
+println("PAAD:")
 display(@benchmark bench_tr_mul_yaad(yaad_x, yaad_y))
 println()
 
 
 function bench_tr_mul_yaad(x1, x2)
     forward(z)
-    YAAD.backward(z)
+    PAAD.backward(z)
     x1.grad, x2.grad
 end
 
