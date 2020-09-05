53a1073e79798445bf797417cb827e40dd903356
Sayantan Das <36279638+ucalyptus@users.noreply.github.com>
2020-09-05 18:07:52 +0530

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -1,4 +1,4 @@
-using Documenter, YAAD
+using Documenter, PAAD
 
 # TODO: use Literate to process examples
 # using Literate
@@ -6,21 +6,21 @@ using Documenter, YAAD
 
 # make documents
 makedocs(
-    modules = [YAAD],
+    modules = [PAAD],
     clean = false,
     format = :html,
-    sitename = "YAAD.jl",
+    sitename = "PAAD.jl",
     linkcheck = !("skiplinks" in ARGS),
     analytics = "UA-89508993-1",
     pages = [
         "Home" => "index.md",
     ],
     html_prettyurls = !("local" in ARGS),
-    html_canonical = "https://rogerluo.me/YAAD.jl/latest/",
+    html_canonical = "https://ucalyptus-plus.github.io/PAAD.jl/latest/",
 )
 
 deploydocs(
-    repo = "github.com/Roger-luo/YAAD.jl.git",
+    repo = "github.com/ucalyptus-plus/PAAD.jl.git",
     target = "build",
     julia = "1.0",
     osname = "osx",
